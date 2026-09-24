05ad36a4fdc2d6526456c902ad4ea1450d6af54d
Chrismon014 <crisbar2504@gmail.com>
2026-09-23 22:42:43 -0500

    Update library URL for EleriumV2

--- Loader.lua
+++ Loader.lua
@@ -13,7 +13,7 @@ getgenv().supported = {
 }
 
 -- Cargar librería UI EleriumV2 desde tu propio repositorio
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Chrismon014/SilenceScripts/main/librarys/eleriumv2.luau"))()
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Chrismon014/Script-muscle-/refs/heads/main/eleriumv2.luau"))()
 local window = library:AddWindow("Silence | Key System", {
     main_color = Color3.fromRGB(138, 0, 0),
     min_size = Vector2.new(400, 260),
